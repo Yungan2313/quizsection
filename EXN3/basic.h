161e3137a0297c3947c322ea04ce74660f0f6549
Yungan2313 <65326996+Yungan2313@users.noreply.github.com>
2023-03-21 11:00:30 +0800

    finish

---
+++ EXN3/basic.h
@@ -0,0 +1,20 @@
+#define SAME 1111
+#define LOWER 2222
+#define HIGHER 8888 
+#define MAXBUF 4096
+#define Nor 1010
+#define TooMany 999
+#define TooLess 111
+#define INFIX 0
+#define PREFIX 1
+#define POSTFIX 2
+#define Unknown 404
+#define Duplicate 22
+#define Rev 101
+#define TRUE 1
+#define FALSE 0
+#define isAddOrMinus(x) #x == "+"?1:0
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
